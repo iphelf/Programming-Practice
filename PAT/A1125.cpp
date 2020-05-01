79a9d8c3ad81e03a4c90c4a3cf0d81b4abe62cba
iphelf <2871618714@qq.com>
2020-05-01 17:36:15 +0800

    sort, STL, lambda

--- PAT/A1125.cpp
+++ PAT/A1125.cpp
@@ -10,17 +10,14 @@ const int MAXN=1e4;
 int N;
 int len[MAXN];
 
-int add(int l,int r) {
-    return (l+r)/2;
-}
-
 int main(void) {
 //    freopen("in.txt","r",stdin);
     scanf("%d",&N);
     for(int i=0; i<N; i++) scanf("%d",&len[i]);
     sort(len,len+N);
     printf("%d\n",
-           accumulate(len+1,len+N,len[0],add));
+           accumulate(len+1,len+N,len[0],
+                      [](int l,int r)->int{return (l+r)/2;}));
     return 0;
 }
 
